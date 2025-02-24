97d2f005f672ba0423fe955e65e0fa0afe8a5cd8
Alishba Nasir <165299398+alishbanasir@users.noreply.github.com>
2025-02-24 16:03:07 +0500

    first commit

---
+++ Day1(challenge)/index.py
@@ -0,0 +1,12 @@
+#Day 1 - Python Challenge: Print Your First Message!
+
+Name="Alishba"
+Father_Name="Nasir"
+# using f"" => f string
+print(f"Hello, Python Learners! 🚀 Today is the first step in our coding journey {Name} {Father_Name}")
+
+#\n => new line
+print(f"WELCOME,\n{Name} {Father_Name}, Let's get started!")
+
+#motivational line
+print("Every great Coder takes the first step in their journey. Let's make this journey amazing together!")
