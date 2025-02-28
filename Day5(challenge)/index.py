c5d3fa84e9814ef54e93b9b4beb042b30f9575a3
Alishba Nasir <165299398+alishbanasir@users.noreply.github.com>
2025-02-28 23:11:36 +0500

    Updated code with new changes

---
+++ Day5(challenge)/index.py
@@ -0,0 +1,16 @@
+#Task 5 Challenge:
+#Aisa Python program likhna hai jo user se ek number lega aur uska sum calculate karega 1 se lekar us number tak. 
+
+#/////// sum challenge ////////
+
+
+number = [10, 20, 10, 20, 10]
+total = sum(number)
+print("Total Sum:", total)
+
+user_number=int(input("enter any number: "))
+sum_result = (user_number * (user_number + 1)) // 2 
+print("Sum from 1 to", user_number, "is:", sum_result)
+
+
+
